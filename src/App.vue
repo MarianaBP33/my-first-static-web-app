f112aa531f0c26c636237d1a5c5d40c0e3f6da62
MarianaB <mariana.bp.33@gmail.com>
2023-03-31 17:20:56 -0600

    final

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div class="center">
+    <h1>Hello {{ value }}!</h1>
+    <h3>Integrantes:</h3>
+    <h4>- Mariana</h4>
+    <h4>- Carolina</h4>
+    <h4>- Alex</h4>
+    <h4>- Santos</h4>
+  </div>
+  
 </template>
 
 <script>
@@ -7,8 +15,17 @@ export default {
   name: "App",
   data() {
     return {
-      value: "People"
+      value: "Profesor"
     };
   }
 };
 </script>
+<style scoped>
+  div{
+    justify-content: center;
+  }
+  .center {
+  text-align: center;
+  border: 3px solid green;
+}
+</style>
